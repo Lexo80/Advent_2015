6e0d3c27012038d05f1aed4f001d9c116cc1ebcb
Rene <stupidmonkey@gmx.de>
2019-03-27 22:41:30 +0100

    add day 1 star 2

---
+++ Day1/AoC_2015_2.py
@@ -0,0 +1,18 @@
+"""
+Advent of code
+Day 1 star 2
+"""
+with open('puzzle2.txt') as f:
+    data = f.read()
+    floor = 0
+    index =0
+    print(len(data))
+    for element in data:
+        index +=1
+        if element == '(':
+            floor +=1
+        elif element == ')':
+            floor -=1
+        if floor == -1:
+            break
+    print(index)
